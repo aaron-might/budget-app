a72394845a5fb81dbbae724376d8f7f2812a57e5
aaron-might <aaronmighty7@gmail.com>
2021-07-17 20:30:06 -0400

    write checkDatabase

--- public/dt.js
+++ public/dt.js
@@ -11,3 +11,20 @@ request.onupgradeneeded = (e) => {
   };
   
   request.onerror = (e) => console.log("Error", e.target.errorCode);
+
+  const checkDatabase = () => {
+    // open a transaction with bs
+    let transaction = db.transaction([bs], 'readwrite');
+    // access your bs object
+    const store = transaction.objectStore(bs);
+    const getAll = store.getAll();
+getAll.onsuccess = () =>{
+    if(getAll.result.length > 0){
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json', 
+      },
+    })
