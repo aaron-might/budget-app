2063857f9f75018d6e1932342a2a8e8070524afe
aaron-might <aaronmighty7@gmail.com>
2021-07-17 20:57:10 -0400

    add addeventlistner function

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,3 +8,12 @@ const FILES_TO_CACHE =[
 const CACHE_NAME = 'static-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache =>{
+            console.log('Your files were successfully pre-cached!');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+);
+self.skipWaiting();
+})
