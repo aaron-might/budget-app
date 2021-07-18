9994c1320c47dac3d136cb81359560b965a0e8bf
aaron-might <aaronmighty7@gmail.com>
2021-07-17 21:21:20 -0400

    fixing bugs

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,14 @@
-const FILES_TO_CACHE =[
+const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/manifest.webmanifest',
-    '/assets/images/icons/icon-192x192.png'
+    '/assets/images/icons/icon-192x192.png',
+    '/assets/images/icons/icon-512x512.png',
+    '/assets/css/styles.css',
+    '/dt.js',
+    '/index.js',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ];
 
 const CACHE_NAME = 'static-cache-v2';
